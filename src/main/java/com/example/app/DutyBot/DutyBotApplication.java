77b9dc49c1e1cfdf7b2d7fe847b15abbf069b8f6
Iliailineya <ilavituk011@gmail.com>
2023-10-26 15:10:28 +0300

    Initial commit

---
+++ src/main/java/com/example/app/DutyBot/DutyBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.app.DutyBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutyBotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/app/DutyBot/entity/User.java
@@ -0,0 +1,9 @@
+package com.example.app.DutyBot.entity;
+
+
+public class User {
+    public long id;
+    public String name;
+    public String birthday;
+    public String duty;
+}
