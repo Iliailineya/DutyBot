5eb4d0b069d7132d59560cfedbc8f1430316b10b
Iliailineya <ilavituk011@gmail.com>
2023-11-14 19:27:00 +0200

    Usual commit

--- src/main/java/com/example/app/DutyBot/util/Storage.java
+++ src/main/java/com/example/app/DutyBot/util/Storage.java
@@ -4,19 +4,16 @@ import java.util.ArrayList;
 
 public class Storage {
     private final ArrayList<String> quoteList;
-    public Storage()
-    {
+
+    public Storage() {
         quoteList = new ArrayList<>();
         quoteList.add("Начинать всегда стоит с того, что сеет сомнения. \n\nБорис Стругацкий.");
         quoteList.add("80% успеха - это появиться в нужном месте в нужное время.\n\nВуди Аллен");
         quoteList.add("Мы должны признать очевидное: понимают лишь те,кто хочет понять.\n\nБернар Вербер");
     }
 
-    public String getRandQuote()
-    {
-        //получаем случайное значение в интервале от 0 до самого большого индекса
-        int randValue = (int)(Math.random() * quoteList.size());
-        //Из коллекции получаем цитату со случайным индексом и возвращаем ее
+    public String getRandQuote() {
+        int randValue = (int) (Math.random() * quoteList.size());
         return quoteList.get(randValue);
     }
 }
